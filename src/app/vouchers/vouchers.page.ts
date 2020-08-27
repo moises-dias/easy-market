f779fecbcfb71ee0cdf60c1bf6f84b3d378b0a29
moises-dias <moisesdepaulodias@gmail.com>
2020-08-27 11:10:57 -0300

    T-12 vouchers working

--- src/app/vouchers/vouchers.page.ts
+++ src/app/vouchers/vouchers.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../firebase.service';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-vouchers',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vouchers.page.scss'],
 })
 export class VouchersPage implements OnInit {
+  vouchers: Observable<any[]>;
+  userMail: string = '';
 
-  constructor() { }
+  constructor(
+    private firebaseService: FirebaseService,
+    private authService: AuthService
+  ) {
+    this.authService.userMail.subscribe(mail => this.userMail = mail);
+    this.vouchers = this.firebaseService.getVouchers(this.userMail);
+   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  discountVoucher(id: string, quantity: number){
+    if (quantity > 0) {
+      this.firebaseService.discountVoucher(id, quantity);
+    }
+    else {
+      this.firebaseService.removeVoucher(id);
+    }
   }
 
 }
